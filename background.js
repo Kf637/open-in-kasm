cea8715c90ca55c1769d2035d5f2962eea8110aa
Kf637 <mail@kf637.tech>
2025-06-04 11:57:17 +0200

    add Norwegian and Swedish translations

--- background.js
+++ background.js
@@ -1,12 +1,26 @@
 chrome.runtime.onInstalled.addListener(() => {
+  const contexts = ["link", "page", "selection"];
   chrome.contextMenus.create({
     id: "open_in_kasm",
-    title: "Open in Kasm",
-    contexts: ["link", "page", "selection"]
+    title: chrome.i18n.getMessage("openIn"),
+    contexts
+  });
+  chrome.contextMenus.create({
+    id: "open_in_kasm_tab",
+    parentId: "open_in_kasm",
+    title: chrome.i18n.getMessage("openInNewTab"),
+    contexts
+  });
+  chrome.contextMenus.create({
+    id: "open_in_kasm_window",
+    parentId: "open_in_kasm",
+    title: chrome.i18n.getMessage("openInNewWindow"),
+    contexts
   });
 });
 
-chrome.contextMenus.onClicked.addListener((info, tab) => {
+chrome.contextMenus.onClicked.addListener((info) => {
+  if (!info.menuItemId.startsWith('open_in_kasm_')) return;
   let url = info.linkUrl || info.pageUrl || info.selectionText;
   if (info.selectionText) {
     url = info.selectionText.startsWith('http') ? info.selectionText : '';
@@ -14,6 +28,10 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (!url) return;
   chrome.storage.sync.get({ domain: 'https://kasm.example.com' }, (items) => {
     const kasmUrl = `${items.domain}/#/go?kasm_url=${encodeURIComponent(url)}`;
-    chrome.tabs.create({ url: kasmUrl });
+    if (info.menuItemId === 'open_in_kasm_window') {
+      chrome.windows.create({ url: kasmUrl });
+    } else {
+      chrome.tabs.create({ url: kasmUrl });
+    }
   });
 });
